9f49dd4e0fd0b9a7bb89619d4127aae5ed9b800f
Arek Mądry <arkadiusz.madry.2000.com>
2023-04-16 22:02:29 +0200

    reformat data generating

--- getPokemonList.py
+++ getPokemonList.py
@@ -6,30 +6,43 @@ import aiohttp
 import requests
 
 
+def get_mapped_types(pok_types):
+    mapped_types = []
+    for pok_type in pok_types:
+        mapped = {"slot": pok_type["slot"], "name": pok_type["type"]["name"]}
+        mapped_types.append(mapped)
+    return mapped_types
+
+
 def get_pokemon_details_reduced(pokemon_details):
     wanted_keys = ["id", "name", "types"]
     image_url = pokemon_details["sprites"]["other"]["official-artwork"]["front_default"]
     pokemon_details_filtered = {key: pokemon_details[key] for key in wanted_keys}
     pokemon_details_filtered.update({"image_url": image_url})
+    pokemon_details_filtered["types"] = get_mapped_types(pokemon_details["types"])
+
     return pokemon_details_filtered
 
 
 async def fetch_pokemon_details(s, pokemon):
-
     async with s.get(pokemon["url"]) as resp:
         pokemon_details: dict = await resp.json()
-        current_pokemon_basic_data = get_pokemon_details_reduced(pokemon_details)
-        with open("./data/pokemon/pokemonDetails" + str(current_pokemon_basic_data["id"]) + ".json", 'w') as fp:
-            json.dump(current_pokemon_basic_data, fp)
+        return get_pokemon_details_reduced(pokemon_details)
 
 
 async def get_pokemon_details_list_from(s, pokemon_basic_request: dict):
     processed_pokemons = 0
+    tasks = []
     for pokemon in pokemon_basic_request["results"]:
-        await asyncio.create_task(fetch_pokemon_details(s, pokemon))
+        task = asyncio.create_task(fetch_pokemon_details(s, pokemon))
+        tasks.append(task)
         processed_pokemons += 1
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("ProcessedPokemons: " + str(processed_pokemons) + "/" +str(pokemon_basic_request["count"]))
+        print("ProcessedPokemons: " + str(processed_pokemons) + "/" + str(pokemon_basic_request["count"]))
+
+    all_pokemon_details = await asyncio.gather(*tasks)
+    with open("./data/pokemon/pokemonDetails.json", 'w') as fp:
+        json.dump(all_pokemon_details, fp)
 
 
 async def main():
