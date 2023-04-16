465142e9182d04aad0a33ba507e1323fe9e425f3
Arek Mądry <arkadiusz.madry.2000.com>
2023-04-16 21:22:13 +0200

    erase data

--- test_place.py
+++ test_place.py
@@ -1,16 +1,44 @@
+import asyncio
 import requests
+import aiohttp
+from timeit import default_timer as timer
 
-requestJson = requests.get(
-    "https://raw.githubusercontent.com/arix2000/Pokedex_Data/develop/data/pokemon/pokemonListPage1.json").json()
 
-pok_range = range(1, 30)
+async def fetch_pokemon(s, pok, from_poke_api):
+    if not from_poke_api:
+        async with s.get(
+                "https://raw.githubusercontent.com/arix2000/Pokedex_Data/develop/data/pokemon/pokemonDetails" + str(
+                    pok) + ".json") as resp:
+            json_resp = await resp.json(content_type=None)
+            return json_resp
+    else:
+        async with s.get("https://pokeapi.co/api/v2/pokemon/" + str(pok)) as resp:
+            json_resp = await resp.json(content_type=None)
+            return json_resp
 
-fetchedPokemons = []
-for pok in pok_range:
-    requestJson = requests.get(
-        "https://raw.githubusercontent.com/arix2000/Pokedex_Data/develop/data/pokemon/pokemonDetails" + str(
-            pok) + ".json").json()
-    fetchedPokemons.append(requestJson)
 
-print(fetchedPokemons)
-print(len(fetchedPokemons))
+async def get_all(s, from_poke_api):
+    pok_range = range(1, 30)
+    print("from poke api? :" + str(from_poke_api))
+    tasks = []
+    for pok in pok_range:
+        task = asyncio.create_task(fetch_pokemon(s, pok, from_poke_api))
+        tasks.append(task)
+    fetched_pokemons = await asyncio.gather(*tasks)
+    print(len(fetched_pokemons))
+
+
+async def main(from_poke_api):
+    async with aiohttp.ClientSession() as session:
+        await get_all(session, from_poke_api)
+
+
+start = timer()
+asyncio.run(main(False))
+end = timer()
+print(end - start)
+print("-----------------------------------------------------------------------------------------------")
+start2 = timer()
+asyncio.run(main(True))
+end2 = timer()
+print(end2 - start2)
